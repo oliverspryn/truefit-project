a55937416fc614a5516889b481f12bb424ceb167
alias65536 <jv789521@gmail.com>
2014-02-11 19:30:50 -0500

    Created Project

---
+++ TrueFitProjectTracker/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrueFitProjectTracker.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+
+            int number = 9000;
+            return View(number);
+        }
+
+    }
+}
