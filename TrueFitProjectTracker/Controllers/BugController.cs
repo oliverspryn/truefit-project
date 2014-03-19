00a9a9d6d7b0f548668368f26ee3487e93f67053
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2014-03-18 23:33:11 -0400

    Added basics of Bug reporter page

---
+++ TrueFitProjectTracker/Controllers/BugController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrueFitProjectTracker.Controllers
+{
+    public class BugController : Controller
+    {
+        //
+        // GET: /Bug/
+
+        public ActionResult Index(int id)
+        {
+            return View();
+        }
+
+    }
+}
