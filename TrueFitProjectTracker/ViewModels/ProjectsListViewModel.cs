ce341feec0c7c34ec2a31e078a206623f5b67ec6
lehmanad1 <lehmanad1@gcc.edu>
2014-03-18 21:08:00 -0400

    Project List Changes Implemented
    Project List on main page now styled, with completion bar in order to
    show client general idea of progress.

--- TrueFitProjectTracker/ViewModels/ProjectsListViewModel.cs
+++ TrueFitProjectTracker/ViewModels/ProjectsListViewModel.cs
@@ -13,11 +13,11 @@ namespace TrueFitProjectTracker.ViewModels
         {
             // TODO: Load Data from JIRA
 
-            this.projectsList.Add(new ProjectEntry(1, "Tree Project", "Reforesters United"));
-            this.projectsList.Add(new ProjectEntry(5, "Tree Project", "Reforesters United"));
-            this.projectsList.Add(new ProjectEntry(7, "Tree Project", "Reforesters United"));
-            this.projectsList.Add(new ProjectEntry(20, "Tree Project", "Reforesters United"));
-            this.projectsList.Add(new ProjectEntry(21, "Tree Project", "Reforesters United"));
+            this.projectsList.Add(new ProjectEntry(10001, "Tree Project", "Reforesters United", 27));
+            this.projectsList.Add(new ProjectEntry(10005, "Mortal Wombats", "Capricornicopia", 78));
+            this.projectsList.Add(new ProjectEntry(10007, "The Chunky Spaghetti Crisis", "Mr. Meatball Maniac", 12));
+            this.projectsList.Add(new ProjectEntry(10020, "The 3/4 Done Club", "QuarterMain Studios",76));
+            this.projectsList.Add(new ProjectEntry(20221, "Whale Nuker/Backpack Problem Solver", "Eli Houston IV",100));
         }
 
         public List<ProjectEntry> projectsList = new List<ProjectEntry>();
@@ -30,11 +30,12 @@ namespace TrueFitProjectTracker.ViewModels
     {
         // represents one project item in a list
         public ProjectEntry() { }
-        public ProjectEntry(int id, string name, string clientName)
+        public ProjectEntry(int id, string name, string clientName, int totalProgress)
         {
             this.id = id;
             this.name = name;
             this.clientName = clientName;
+            this.totalProgress = totalProgress;
         }
 
 
@@ -55,11 +56,16 @@ namespace TrueFitProjectTracker.ViewModels
             get { return clientName; }
             set { clientName = value; }
         }
-        
+        public int TotalProgress
+        {
+            get { return totalProgress; }
+            set { totalProgress = value; }
+        }   
 
         int id = 0;
+        int totalProgress = 0;
         string name = "";
         string clientName;
-
+        
     }
 }
