fb61a05e51a9623dac7ce0390a0c6d869998071b
jeffvd-a216 <jv789521@gmail.com>
2014-04-23 12:50:33 -0400

    repaired good tasks check

--- TrueFitProjectTracker/Controllers/HomeController.cs
+++ TrueFitProjectTracker/Controllers/HomeController.cs
@@ -52,76 +52,88 @@ namespace TrueFitProjectTracker.Controllers
                         tasklist.AddRange(value.Tasks.Where(task => task.Issue == type));
                         return tasklist;
                     });
-                var goodTasks = tasksList.Where(task => task.Created != null // no unbegun tasks please!
-                    && (task.DueDate != null || task.ResolutionDate != new DateTime())).ToList(); // need either or.
+                var goodTasks = tasksList.Where(task => task.Created != new DateTime(1970,1,1) // no unbegun tasks please!
+                    && (task.DueDate != new DateTime(1970,1,1) || task.ResolutionDate != new DateTime(1970,1,1))).ToList(); // need either or.
 
-                // month, year dates
-                var startDate = DateTime.Now.Date; //goodTasks
-                //    .Select(task => task.Created).Min().Date;
-                startDate = startDate.AddDays(-startDate.Day + 1);
-                var start = new Tuple<int, int>(startDate.Month, startDate.Year);
-                
-                // the last month with activities in it
-                var endDate = goodTasks
-                    .Select(task => task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate)
-                    .Max().Date;
-                endDate = endDate.AddDays(-endDate.Day + 1);
-                // on the x axis, include extra month
-                var end = new Tuple<int, int>(endDate.AddMonths(1).Month, endDate.Year);
 
-                var datesList = Enumerable
-                    .Range(0, 1+ (endDate.Year - startDate.Year) * 12 + endDate.Month - startDate.Month)
-                    .Select(i => startDate.AddMonths(i))
-                    .ToList();
+                List<double> taskData = new List<double>();
+                var start = new Tuple<int, int>((int)DateTime.Now.Month, (int)DateTime.Now.Year);
+                var end = new Tuple<int, int>((int)DateTime.Now.Month + 1, (int)DateTime.Now.Year);
 
-                // list of doubls to output to view model in one nice linq query
-                List<double> taskData = datesList
-                    .Select(monthYear =>
-                    {   // get work for each month.
-                        // where this month has something to do with this task
-                        return tasksList.Where(task => task.Created < monthYear.AddMonths(1)
-                            && (task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate) >= monthYear)
-                            // get man hours done in this month, final block.
-                            .Select(task =>
-                            {   // get ratio of % of task being done in this month
-                                double ratioInThisMonth = 0;
-                                if (task.Created < monthYear)
-                                {
-                                    if ((task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate) < monthYear.AddMonths(1))
-                                    {   // task  |-----|
-                                        // month    |----|
-                                        ratioInThisMonth = 1d
-                                            * (task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate).Subtract(monthYear).Days
-                                            / (task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate).Subtract(task.Created).Days;
+
+                if (goodTasks.Count != 0)
+                {
+
+                    // month, year dates
+                    var startDate = goodTasks
+                        .Select(task => task.Created).Min().Date;
+                    startDate = startDate.AddDays(-startDate.Day + 1);
+                    start = new Tuple<int, int>(startDate.Month, startDate.Year);
+
+                    // the last month with activities in it
+                    var endDate = goodTasks
+                        .Select(task => task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate)
+                        .Max().Date;
+                    endDate = endDate.AddDays(-endDate.Day + 1);
+                    // on the x axis, include extra month
+                    end = new Tuple<int, int>(endDate.AddMonths(1).Month, endDate.Year);
+
+                    var datesList = Enumerable
+                        .Range(0, 1 + (endDate.Year - startDate.Year) * 12 + endDate.Month - startDate.Month)
+                        .Select(i => startDate.AddMonths(i))
+                        .ToList();
+
+                    // list of doubls to output to view model in one nice linq query
+                    taskData = datesList
+                        .Select(monthYear =>
+                        {   // get work for each month.
+                            // where this month has something to do with this task
+                            return tasksList.Where(task => task.Created < monthYear.AddMonths(1)
+                                && (task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate) >= monthYear)
+                                // get man hours done in this month, final block.
+                                .Select(task =>
+                                {   // get ratio of % of task being done in this month
+                                    double ratioInThisMonth = 0;
+                                    if (task.Created < monthYear)
+                                    {
+                                        if ((task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate) < monthYear.AddMonths(1))
+                                        {   // task  |-----|
+                                            // month    |----|
+                                            ratioInThisMonth = 1d
+                                                * (task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate).Subtract(monthYear).Days
+                                                / (task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate).Subtract(task.Created).Days;
+                                        }
+                                        else
+                                        {   // task  |---------|
+                                            // month    |----|
+                                            ratioInThisMonth = 1d
+                                                * monthYear.AddMonths(1).Subtract(monthYear).Days
+                                                / (task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate).Subtract(task.Created).Days;
+                                        }
                                     }
                                     else
-                                    {   // task  |---------|
-                                        // month    |----|
-                                        ratioInThisMonth = 1d
-                                            * monthYear.AddMonths(1).Subtract(monthYear).Days
-                                            / (task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate).Subtract(task.Created).Days;
+                                    {
+                                        if ((task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate) < monthYear.AddMonths(1))
+                                        {   // task       |-|
+                                            // month    |----|
+                                            ratioInThisMonth = 1d; // 100%
+                                        }
+                                        else
+                                        {   // task       |----|
+                                            // month    |----|
+                                            ratioInThisMonth = 1d
+                                                * monthYear.Subtract(task.Created).Days
+                                                / (task.ResolutionDate != new DateTime(1970, 1, 1) ? task.ResolutionDate : task.DueDate).Subtract(task.Created).Days;
+                                        }
                                     }
-                                }
-                                else
-                                {
-                                    if ((task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate) < monthYear.AddMonths(1))
-                                    {   // task       |-|
-                                        // month    |----|
-                                        ratioInThisMonth = 1d; // 100%
-                                    }
-                                    else
-                                    {   // task       |----|
-                                        // month    |----|
-                                        ratioInThisMonth = 1d
-                                            * monthYear.Subtract(task.Created).Days
-                                            / (task.ResolutionDate != new DateTime() ? task.ResolutionDate : task.DueDate).Subtract(task.Created).Days;
-                                    }
-                                }
-                                // get actual man hours from task effort and percentage... whoops, don't have em. 
-                                return ratioInThisMonth * monthYear.AddMonths(1).Subtract(monthYear).Days;
-                            })
-                            .Sum();
-                    }).ToList();
+                                    // get actual man hours from task effort and percentage... whoops, don't have em. 
+                                    return ratioInThisMonth * monthYear.AddMonths(1).Subtract(monthYear).Days;
+                                })
+                                .Sum();
+                        }).ToList();
+                }
+
+
                 // get recent tasks
                 List<int> recentTasksData = Enumerable.Range(0, 7) // 0 ... 6
                     .Select(n => DateTime.Now.Date.AddDays(-7 + n))
@@ -130,7 +142,7 @@ namespace TrueFitProjectTracker.Controllers
                 // completion statistics
                 int recentCompletedCount = tasksList.Count(task => task.ResolutionDate.Date.CompareTo(DateTime.Now.Date) <= 0
                     && task.ResolutionDate.Date.CompareTo(DateTime.Now.Date.AddDays(-7)) >= 0);
-                int completedCount = tasksList.Count(task => task.ResolutionDate != new DateTime());
+                int completedCount = tasksList.Count(task => task.ResolutionDate != new DateTime(1970,1,1));
                 int remainingCount = tasksList.Count() - completedCount;
 
                 double progress = (double)(completedCount) / (remainingCount + completedCount) * 100; // percent
