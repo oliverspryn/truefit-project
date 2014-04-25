4745094c035ac3def49d1c1ff129d872bfcb102c
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2014-04-25 13:00:15 -0400

    Re-enabled ASP.NET exception filter.

--- TrueFitProjectTracker/Global.asax.cs
+++ TrueFitProjectTracker/Global.asax.cs
@@ -19,7 +19,7 @@ namespace TrueFitProjectTracker
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
-            //FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
